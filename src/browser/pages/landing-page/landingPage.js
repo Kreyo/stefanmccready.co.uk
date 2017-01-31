c20f9d6a74e6d3701952589a9fc83fd04ca68382
Stefan <Stefan.McCready@sky.uk>
2017-01-31 22:34:39 +0000

    Added font hierarchy

--- src/browser/pages/landing-page/landingPage.js
+++ src/browser/pages/landing-page/landingPage.js
@@ -3,7 +3,12 @@ import ReactDOM from 'react-dom';
 
 const LandingPage = () =>
     <div className="container">
-      <h1 className="text-center">Stefan McCready</h1>
+      <h1 className="text-center heading-large">Stefan McCready</h1>
+      <h2 className="text-center heading-medium">Stefan McCready</h2>
+      <h3 className="text-center heading-small">Stefan McCready</h3>
+      <p className="text-large text-center">Large Text</p>
+      <p className="text-medium text-center">Large Text</p>
+      <p className="text-small text-center">Large Text</p>
     </div>
 
 export default LandingPage;
