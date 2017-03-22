91f7eab00ee4d90bb1a0b737da9efc762a6ab643
StefanMcCready <stefan.mccready@sky.uk>
2017-03-22 22:10:08 +0000

    cleaned up files and added a couple of styles

--- src/browser/pages/landing-page/landingPage.js
+++ src/browser/pages/landing-page/landingPage.js
@@ -2,8 +2,19 @@ import React, { PropTypes } from 'react';
 import ReactDOM from 'react-dom';
 
 const LandingPage = () =>
-    <div className="text-center">
-        <h1>Hello</h1>
+    <div>
+        <div className="panel">
+            <div className="panel__content">
+                <h1 className="heading-large margin-bottom-small">STEFAN MCCREADY.</h1>
+                <h2 className="heading-medium">WEBSITE DESIGNER AND DEVELOPER.</h2>
+            </div>
+        </div>
+        <div className="panel panel--alternative">
+            <div className="panel__content">
+                <h1 className="heading-large margin-bottom-small">STEFAN MCCREADY.</h1>
+                <h2 className="heading-medium">WEBSITE DESIGNER AND DEVELOPER.</h2>
+            </div>
+        </div>
     </div>
 
 
