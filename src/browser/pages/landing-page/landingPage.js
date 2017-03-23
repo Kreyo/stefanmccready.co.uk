185c64b744d36270fff29e28a45bbd2fb36b6e3a
StefanMcCready <stefan.mccready@sky.uk>
2017-03-23 22:37:51 +0000

    Created icon bar

--- src/browser/pages/landing-page/landingPage.js
+++ src/browser/pages/landing-page/landingPage.js
@@ -4,13 +4,28 @@ import ReactDOM from 'react-dom';
 const LandingPage = () =>
     <div>
         <div className="panel">
-            <div className="panel__content">
+            <div className="icon-bar">
+                <div className="icon-bar__left">
+                    <i className="fa fa-bars icon-bar___icon" aria-hidden="true"></i>
+                </div>
+                <div className="icon-bar__right">
+                    <i className="fa fa-linkedin icon-bar___icon" aria-hidden="true"></i>
+                    <i className="fa fa-twitter icon-bar___icon" aria-hidden="true"></i>
+                </div>
+            </div>
+            <div className="panel__content text-center">
                 <h1 className="heading-large margin-bottom-small">STEFAN MCCREADY.</h1>
                 <h2 className="heading-medium">WEBSITE DESIGNER AND DEVELOPER.</h2>
             </div>
         </div>
         <div className="panel panel--alternative">
             <div className="panel__content">
+                <h1 className="heading-large margin-bottom-small">STEFAN MCCREADY.</h1>
+                <h2 className="heading-medium">WEBSITE DESIGNER AND DEVELOPER.</h2>
+            </div>
+        </div>
+        <div className="panel">
+            <div className="panel__content text-right">
                 <h1 className="heading-large margin-bottom-small">STEFAN MCCREADY.</h1>
                 <h2 className="heading-medium">WEBSITE DESIGNER AND DEVELOPER.</h2>
             </div>
