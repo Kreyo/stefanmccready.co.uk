174c9532788b611db79a420e8277f5d169d0a579
StefanMcCready <stefan.mccready@sky.uk>
2017-04-01 23:50:35 +0100

    Mockup cv download button

--- src/browser/pages/landing-page/landingPage.js
+++ src/browser/pages/landing-page/landingPage.js
@@ -20,7 +20,7 @@ const LandingPage = () =>
                 <div>
                     <h1 className="heading-large margin-bottom-small">STEFAN MCCREADY</h1>
                     <h2 className="heading-small margin-bottom">DEVELOPER | DESIGNER</h2>
-                    <button className="btn btn--alternative panel__btn"><i className="fa fa-arrow-down" aria-hidden="true"></i></button>
+                    <button className="btn btn--alternative btn--circle panel__btn"><i className="fa fa-arrow-down" aria-hidden="true"></i></button>
                 </div>
             </div>
         </div>
@@ -29,7 +29,8 @@ const LandingPage = () =>
                 <div className="panel__content">
                     <h1 className="heading-medium margin-bottom-large">HELLO | BONJOUR | 你好</h1>
                     <img className="panel__image margin-bottom" src={require('../../images/stefan.png')} />
-                    <p className="text-large panel__description">My name is Stefan and I currently work as a front-end developer at <strong>Sky in Leeds, UK</strong> and will code at every possible oppurtunity. Some say I have an addiction to it.</p>
+                    <p className="text-large panel__description margin-bottom">My name is Stefan and I currently work as a front-end developer at <strong>Sky in Leeds, UK</strong> and will code at every possible oppurtunity. Some say I have an addiction to it.</p>
+                    <button className="btn btn--standard"><span className="margin-right-small">DOWNLOAD MY CV</span><i className="fa fa-download" aria-hidden="true"></i></button>
                 </div>
             </div>
         </div>
