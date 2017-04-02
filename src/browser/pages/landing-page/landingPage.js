bdf033f249b7c78371e694238313beef307e6447
StefanMcCready <stefan.mccready@sky.uk>
2017-04-02 15:42:49 +0100

    Completed state modifier for locked navbar

--- src/browser/pages/landing-page/landingPage.js
+++ src/browser/pages/landing-page/landingPage.js
@@ -3,19 +3,19 @@ import ReactDOM from 'react-dom';
 
 const LandingPage = () =>
     <div>
-        <div className="panel panel--alternative">
-            <div className="icon-bar">
-                <div className="icon-bar__left">
-                    <button className="icon-bar__icon">
-                        <i className="fa fa-bars" aria-hidden="true"></i>
-                    </button>
-                </div>
-                <div className="icon-bar__right">
-                    <button className="icon-bar__icon">
-                        <i className="fa fa-linkedin" aria-hidden="true"></i>
-                    </button>
-                </div>
+        <div className="icon-bar">
+            <div className="icon-bar__left">
+                <button className="icon-bar__icon">
+                    <i className="fa fa-bars" aria-hidden="true"></i>
+                </button>
             </div>
+            <div className="icon-bar__right">
+                <button className="icon-bar__icon">
+                    <i className="fa fa-linkedin" aria-hidden="true"></i>
+                </button>
+            </div>
+        </div>
+        <div className="panel panel--alternative">
             <div className="panel__container text-center">
                 <div>
                     <h1 className="heading-large margin-bottom-small">STEFAN MCCREADY</h1>
