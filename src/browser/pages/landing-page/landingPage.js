f480b947f178cee9ce18d303a4b3618465413209
StefanMcCready <stefan.mccready@sky.uk>
2017-04-09 11:18:20 +0100

    Added other panels

--- src/browser/pages/landing-page/landingPage.js
+++ src/browser/pages/landing-page/landingPage.js
@@ -46,7 +46,27 @@ const LandingPage = () =>
         <div className="panel panel--alternative text-center">
             <div className="panel__container">
                 <div className="panel__content">
-                    <h1 className="heading-medium margin-bottom-large">HELLO | BONJOUR | 你好</h1>
+                    <h1 className="heading-medium margin-bottom-large">THINGS I DO</h1>
+                    <p className="text-large panel__description margin-bottom">5 years as a bedroom coder and working in the industry has taight me a few things, below is the simplest way to show the some of the skills that I do have.</p>
+                    <div className="section group">
+                        <div className="col span_1_of_3">
+                            Hello
+                        </div>
+                        <div className="col span_1_of_3">
+                            Hello
+                        </div>
+                        <div className="col span_1_of_3">
+                            Hello
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <button className="btn btn--standard btn--circle panel__btn"><i className="fa fa-chevron-down" aria-hidden="true"></i></button>
+        </div>
+        <div className="panel panel--stnadard text-center">
+            <div className="panel__container">
+                <div className="panel__content">
+                    <h1 className="heading-medium margin-bottom-large">CONTACT-Y STUFF</h1>
                     <img className="panel__image margin-bottom" src={require('../../images/stefan.png')} alt="Stefan McCready" />
                     <p className="text-large panel__description margin-bottom">My name is Stefan and I currently work as a front-end developer at <strong>Sky in Leeds, UK</strong> and will code at every possible oppurtunity. Some say I have an addiction to it.</p>
                 </div>
