d41c21d576a5227e8b6e1b229f88667be2c484f9
StefanMcCready <stefan.mccready@sky.uk>
2017-04-09 16:42:27 +0100

    Fixed lint script

--- src/browser/components/panel/__test__/panel.spec.js
+++ src/browser/components/panel/__test__/panel.spec.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { shallow, describe, it, expect } from 'enzyme';
+import { shallow } from 'enzyme';
+import { expect } from 'expect';
 import Panel from '../panel';
 
 describe('Panel', () => {
