32613b9120cba0412efd8a3d20287799bdc4b773
StefanMcCready <stefan.mccready@sky.uk>
2017-04-09 12:01:32 +0100

    Created base component

---
+++ src/browser/components/panel/panel.js
@@ -0,0 +1,50 @@
+import React, { PropTypes } from "react";
+
+const Panel = ({ children, modifier, advanceButton, iconBar }) => {
+
+    let advanceModifier;
+    
+    const classNames=(`panel panel--${modifier}`);
+
+    if (modifier === 'alternative') {
+        advanceModifier = 'standard';
+    } else {
+        advanceModifier = 'alternative';
+    }
+
+    const advanceClassNames=(`btn btn--${advanceModifier} btn--circle panel__btn`);
+
+    const IconBarComonent = (
+        <div className="icon-bar">
+            <div className="icon-bar__left">
+                <button className="icon-bar__icon">
+                    <i className="fa fa-bars" aria-hidden="true"></i>
+                    <span>MENU</span>
+                </button>
+            </div>
+            <div className="icon-bar__right">
+                <button className="icon-bar__icon">
+                    <i className="fa fa-linkedin" aria-hidden="true"></i>
+                </button>
+            </div>
+        </div>
+    );
+
+    return(
+        <div className={classNames}>
+            { iconBar ? IconBarComonent : null }
+            <div className="panel__container">
+                <div className="panel__content">
+                    {children}
+                </div>
+            </div>
+            {advanceButton ? 
+                <button className={advanceClassNames}>
+                    <i className="fa fa-chevron-down" aria-hidden="true"></i>
+                </button> : null 
+            }
+        </div>        
+    );
+};
+
+export default Panel;
--- src/browser/pages/landing-page/landingPage.js
+++ src/browser/pages/landing-page/landingPage.js
@@ -1,24 +1,10 @@
 import React, { PropTypes } from 'react';
 import ReactDOM from 'react-dom';
+import Panel from '../../components/panel/panel';
 
 const LandingPage = () =>
     <div>
-        <div className="panel panel--alternative text-center">
-            <div className="icon-bar">
-                <div className="icon-bar__left">
-                    <button className="icon-bar__icon">
-                        <i className="fa fa-bars" aria-hidden="true"></i>
-                        <span>MENU</span>
-                    </button>
-                </div>
-                <div className="icon-bar__right">
-                    <button className="icon-bar__icon">
-                        <i className="fa fa-linkedin" aria-hidden="true"></i>
-                    </button>
-                </div>
-            </div>
-            <div className="panel__container">
-                <div>
+        <Panel modifier="alternative" advanceButton iconBar>
                     <h1 className="panel__heading margin-bottom margin-top-small">STEFAN MCCREADY</h1>
                     <div className="panel__heading-set margin-bottom">
                         <p className="panel__subheading">DESIGNER</p>
@@ -29,49 +15,32 @@ const LandingPage = () =>
                         <button className="btn btn--alternative">DOWNLOAD MY RÈSUMÈ</button>
                         <button className="btn btn--alternative">CONTACT ME</button>
                     </div>
+        </Panel>
+        <Panel modifier="standard" advanceButton>
+            <h1 className="heading-medium margin-bottom-large">HELLO | BONJOUR | 你好</h1>
+            <img className="panel__image margin-bottom" src={require('../../images/stefan.png')} alt="Stefan McCready" />
+            <p className="text-large panel__description margin-bottom">My name is Stefan and I currently work as a front-end developer at <strong>Sky in Leeds, UK</strong> and will code at every possible oppurtunity. Some say I have an addiction to it.</p>
+        </Panel>
+        <Panel modifier="alternative" advanceButton>
+            <h1 className="heading-medium margin-bottom-large">THINGS I DO</h1>
+            <p className="text-large panel__description margin-bottom">5 years as a bedroom coder and working in the industry has taight me a few things, below is the simplest way to show the some of the skills that I do have.</p>
+            <div className="section group">
+                <div className="col span_1_of_3">
+                    Hello
+                </div>
+                <div className="col span_1_of_3">
+                    Hello
+                </div>
+                <div className="col span_1_of_3">
+                    Hello
                 </div>
             </div>
-            <button className="btn btn--standard btn--circle panel__btn"><i className="fa fa-chevron-down" aria-hidden="true"></i></button>
-        </div>
-        <div className="panel text-center">
-            <div className="panel__container">
-                <div className="panel__content">
-                    <h1 className="heading-medium margin-bottom-large">HELLO | BONJOUR | 你好</h1>
-                    <img className="panel__image margin-bottom" src={require('../../images/stefan.png')} alt="Stefan McCready" />
-                    <p className="text-large panel__description margin-bottom">My name is Stefan and I currently work as a front-end developer at <strong>Sky in Leeds, UK</strong> and will code at every possible oppurtunity. Some say I have an addiction to it.</p>
-                </div>
-            </div>
-            <button className="btn btn--alternative btn--circle panel__btn"><i className="fa fa-chevron-down" aria-hidden="true"></i></button>
-        </div>
-        <div className="panel panel--alternative text-center">
-            <div className="panel__container">
-                <div className="panel__content">
-                    <h1 className="heading-medium margin-bottom-large">THINGS I DO</h1>
-                    <p className="text-large panel__description margin-bottom">5 years as a bedroom coder and working in the industry has taight me a few things, below is the simplest way to show the some of the skills that I do have.</p>
-                    <div className="section group">
-                        <div className="col span_1_of_3">
-                            Hello
-                        </div>
-                        <div className="col span_1_of_3">
-                            Hello
-                        </div>
-                        <div className="col span_1_of_3">
-                            Hello
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <button className="btn btn--standard btn--circle panel__btn"><i className="fa fa-chevron-down" aria-hidden="true"></i></button>
-        </div>
-        <div className="panel panel--stnadard text-center">
-            <div className="panel__container">
-                <div className="panel__content">
-                    <h1 className="heading-medium margin-bottom-large">CONTACT-Y STUFF</h1>
-                    <img className="panel__image margin-bottom" src={require('../../images/stefan.png')} alt="Stefan McCready" />
-                    <p className="text-large panel__description margin-bottom">My name is Stefan and I currently work as a front-end developer at <strong>Sky in Leeds, UK</strong> and will code at every possible oppurtunity. Some say I have an addiction to it.</p>
-                </div>
-            </div>
-        </div>
+        </Panel>
+        <Panel modifier="standard">
+            <h1 className="heading-medium margin-bottom-large">CONTACT-Y STUFF</h1>
+            <img className="panel__image margin-bottom" src={require('../../images/stefan.png')} alt="Stefan McCready" />
+            <p className="text-large panel__description margin-bottom">My name is Stefan and I currently work as a front-end developer at <strong>Sky in Leeds, UK</strong> and will code at every possible oppurtunity. Some say I have an addiction to it.</p>
+        </Panel>
     </div>
 
 export default LandingPage;
