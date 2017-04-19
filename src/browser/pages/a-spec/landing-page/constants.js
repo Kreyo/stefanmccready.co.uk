59dc32a7835c78696725bad6e511c7d9799370d7
Stefan <Stefan.McCready@sky.uk>
2017-04-19 22:02:57 +0100

    Moved content from b to a landing page

--- src/browser/pages/a-spec/landing-page/constants.js
+++ src/browser/pages/a-spec/landing-page/constants.js
@@ -1,5 +1,13 @@
+export const linksList = [
+    { route: '/about', title: 'ABOUT ME' },
+    { route: '/portflio', title: 'PORTFOLIO' },
+    { route: '/contact', title: 'CONTACT ME' },
+];
+
 export const skillsList = [
     { icon: 'code', title: 'WEBSITE-DEVELOPMENT' },
     { icon: 'pencil', title: 'WEBSITE-DESIGN' },
     { icon: 'universal-access', title: 'ACCESSIBILITY' },
 ];
+
+export const breakdownCopy = "5 years as a bedroom coder and working in the industry has taught me a few things, below is the simplest way to show the some of the skills that I do have:";
--- src/browser/pages/a-spec/landing-page/landingPage.js
+++ src/browser/pages/a-spec/landing-page/landingPage.js
@@ -1,37 +1,37 @@
 import React from 'react';
 import Panel from '../../../components/panel/panel';
+import NavBar from '../../../components/navbar/navbar';
 import SkillsBreakdown from '../../../components/skills-breakdown/skillsBreakdown';
+import { skillsList, breakdownCopy, linksList } from './constants';
 import Footer from '../../../components/footer/footerWrapper';
 import footerData from '../../../config/footerData.json';
-import { skillsList, breakdownCopy } from './constants';
 
-import IconBar from '../../../components/icon-bar/iconBar';
-
-const iconHeader = (<IconBar menuText="MENU" socialLink="https://www.linkedin.com/in/stefan-mccready-11507297/" socialLogo="linkedin" />);
+const customPanelBar = (<NavBar linkArray={linksList} />);
 
 const LandingPage = () =>
     <div>
-        <Panel modifier="alternative" advanceButton customBar={iconHeader}>
-            <h1 className="panel__heading margin-bottom margin-top-small">STEFAN MCCREADY</h1>
+        <Panel modifier="alternative" customBar={customPanelBar}>
             <div className="panel__heading-set margin-bottom">
-                <p className="panel__subheading">DESIGNER</p>
-                <p className="panel__subheading">DEVELOPER</p>
-                <p className="panel__subheading">MENTOR</p>
+                <p className="panel__heading">DESIGNER</p>
+                <p className="panel__heading">DEVELOPER</p>
+                <p className="panel__heading">MENTOR</p>
             </div>
+            <p className="panel__description">
+                A lot of these website seem to have some text written around this area giving a brief summary about who they are etc. Not me, I just wanted to say that you shouldnt put Pineapple on a Pizza.
+            </p>
             <div className="panel__btn-set margin-top-large">
                 <button className="btn btn--alternative">DOWNLOAD MY RÈSUMÈ</button>
                 <button className="btn btn--alternative">CONTACT ME</button>
             </div>
         </Panel>
-        <Panel modifier="standard" advanceButton>
-            <h2 className="panel__heading margin-bottom-large">HELLO | BONJOUR | 你好</h2>
-            <img className="panel__image margin-bottom" src={require('../../../images/stefan.png')} alt="Stefan McCready" />
+        <Panel modifier="standard" isNarrow imgPanel imgSrc={require('../../../images/me.png')} imgAlt="Me">
+            <h2 className="panel__heading margin-bottom-large margin-top-large">HELLO | BONJOUR | 你好</h2>
             <p className="text-large panel__description margin-bottom">
                 My name is Stefan and I currently work as a front-end developer at <strong>Sky in Leeds, UK</strong> and will code at every possible oppurtunity. Some say I have an addiction to it.
             </p>
         </Panel>
-        <Panel modifier="alternative" advanceButton>
-            <h2 className="panel__heading margin-bottom-large">THINGS I DO</h2>
+        <Panel modifier="alternative" isMedium>
+            <h2 className="panel__heading margin-bottom-large">MY AREAS OF EXPERTISE:</h2>
             <SkillsBreakdown skillsArray={skillsList} breakdownText={breakdownCopy} />
         </Panel>
         <Panel modifier="standard" isNarrow>
