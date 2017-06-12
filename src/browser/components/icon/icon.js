2e6c4ff53bc48ed73ff2283c7a4042e5338cd9c1
StefanMcCready <stefan.mccready@sky.uk>
2017-06-12 20:28:38 +0100

    Icons now work as links

--- src/browser/components/icon/icon.js
+++ src/browser/components/icon/icon.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
 
-const Icon = ({ iconName, setItem }) => {
+const Icon = ({ iconName, setItem, link }) => {
     const iconClassnames = classNames({
         fa: true,
         [`fa-${iconName}`]: true,
@@ -11,13 +11,23 @@ const Icon = ({ iconName, setItem }) => {
 
     return (
         <div className="icon-set__item-container">
-            <i className={iconClassnames} />
+            {link ? 
+                <a href={link} target="_blank">
+                    <i className={iconClassnames} />
+                </a> :
+                <i className={iconClassnames} />
+            }
         </div>
     );
 };
 
+Icon.defaultProps = {
+    link: '',
+}
+
 Icon.propTypes = {
     iconName: PropTypes.string.Required,
+    link: PropTypes.string,
 };
 
 export default Icon; 
